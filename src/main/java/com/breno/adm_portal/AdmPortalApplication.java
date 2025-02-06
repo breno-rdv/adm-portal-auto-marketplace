3f257a9e03bb3148f82fcc3cc4b0cb619117911a
Breno do Val <brenorvale@gmail.com>
2025-02-05 23:47:18 -0300

    chore: add project boilerplate

---
+++ src/main/java/com/breno/adm_portal/AdmPortalApplication.java
@@ -0,0 +1,13 @@
+package com.breno.adm_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breno/adm_portal/AdmPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.adm_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
